9364ae0a89b092764448a264ae86dcb3b0fae63a
benkoehlL <feynmandiagramateur@gmail.com>
2024-03-06 15:58:26 +0100

    integration of geolocation

--- Mapty App/script.js
+++ Mapty App/script.js
@@ -1,12 +1,26 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { longitude } = position.coords;
+      const { latitude } = position.coords;
+      let url = `https://www.google.com/maps/@${latitude},${longitude}`;
+      console.log(url);
+    },
+    function () {
+      alert("Could not get your position.");
+    }
+  );
+}
