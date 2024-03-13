0b1fc98f9120e8bf41fddd202222b16030e13dd2
benkoehlL <feynmandiagramateur@gmail.com>
2024-03-13 15:31:34 +0100

    restructured code to fit MVC architecture

---
+++ Forkify/src/js/model.js
@@ -0,0 +1,24 @@
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
+import recipeView from "./views/recipeView";
+export const state = { recipe: {} };
+
+export const loadRecipe = async function () {
+  try {
+    const data = awaitgetJSON(`${API_URL}/${id}`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    throw err;
+  }
+};
